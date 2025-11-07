f3bfe713fb3f75b6429c8e207f69804c07a53982
Pradeep <Pradeepverma249403@gmail.com>
2025-11-07 19:24:28 +0530

    rebuild for netlify fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/pehal-ngo/",
+//   build: {
+//     outDir: "dist",
+//     assetsDir: "assets",
+//   },
+//   publicDir: "public",
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// 🧠 This config works for both GitHub & Netlify
+const isGithub = process.env.VITE_GITHUB === "true";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/pehal-ngo/",
+  base: isGithub ? "/pehal-ngo/" : "/", // ✅ "/" = Netlify fix, not "./"
   build: {
     outDir: "dist",
     assetsDir: "assets",
